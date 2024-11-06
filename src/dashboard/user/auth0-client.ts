e29b2194fad83cfb5aa510032bad3660a7d256d4
IgnacioRametUTN <129917103+IgnacioRametUTN@users.noreply.github.com>
2024-11-06 11:42:57 -0300

    Create auth0-client.ts

---
+++ src/dashboard/user/auth0-client.ts
@@ -0,0 +1,7 @@
+import { ManagementClient } from 'auth0';
+
+var management = new ManagementClient({
+  domain: 'utntestrecursivos.us.auth0.com',
+  clientId: 'hA8Pui32zq3p7yXNlqr53gQ88LUuvZ7x',
+  clientSecret: 'PXml1O7iB3OCU4u4aER3rgYuzTil2yycIS_aWliQqgBoBX5TtDN3U1GrdatW1COi',
+});
